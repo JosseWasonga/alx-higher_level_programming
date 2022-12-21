bea8225d000063546eba14ed62bc720204c66a2d
JosseWasonga <jossewasonga@gmail.com>
2022-12-21 21:42:50 +0300

    warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -8,4 +8,4 @@ console.log('JavaScript is amazing');
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
->>>>>>> d995a5d7839f6d37bb9a7c6cc94ed56a7a2f5d6c
+>>>>>>> 
